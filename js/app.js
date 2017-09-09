9c0e8041d7261d4d1aa1a8d4885c54c3d5601907
Matt Dellac <matt@dellac.org>
2017-09-09 17:04:52 +0100

    add more content

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var myElement = document.querySelector("header");
+// construct an instance of Headroom, passing the element
+
+var headroom  = new Headroom(myElement);
+// initialise
+headroom.init(); 
