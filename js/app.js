93c31d82fab1d53867a67d094190268e3f8f6c2b
Matt Dellac <matt@dellac.org>
2017-09-10 17:35:33 +0100

    improve mobileversion

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var myElement = document.querySelector("header");
+var myElement = document.querySelector('body');
 // construct an instance of Headroom, passing the element
 
 var headroom  = new Headroom(myElement);
